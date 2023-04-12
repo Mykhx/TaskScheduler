c0cb9e41084f9f596eff42e20fd14de284619cfd
mykhx <mkehry92@gmail.com>
2023-04-12 17:17:16 +0200

    Added more test for ScheduledTask

--- test/TaskSchedulerTest/ScheduledTaskTest.cpp
+++ test/TaskSchedulerTest/ScheduledTaskTest.cpp
@@ -2,13 +2,23 @@
 #include "TaskScheduler/ScheduledTask.h"
 
 TEST(ScheduledTask, defaultConstructorSetsExecutionTimeToNow) {
-
     auto beforeTaskCreation = timeProvider::now();
-    auto task = ScheduledTask();
+    auto scheduledTask = ScheduledTask();
     auto afterTaskCreation = timeProvider::now();
 
-    auto executionTime = task.getExecutionTime();
+    auto executionTime = scheduledTask.getExecutionTime();
 
     EXPECT_GE(executionTime, beforeTaskCreation);
     EXPECT_LE(executionTime, afterTaskCreation);
+}
+
+TEST(ScheduledTask, canBeInitializedWithTimeAndTask) {
+    timePoint timePoint1 = timeProvider::now();
+    int value = 0;
+    auto increaseValue = [&value](){value = 1;};
+    auto scheduledTask = ScheduledTask(std::move(increaseValue), timePoint1);
+
+    EXPECT_EQ(scheduledTask.getExecutionTime(), timePoint1);
+    scheduledTask();
+    EXPECT_EQ(value, 1);
 }
