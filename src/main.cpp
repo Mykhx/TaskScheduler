0e959618b743e8269d47d12587930b4045008787
mykhx <mkehry92@gmail.com>
2023-04-15 21:29:15 +0200

    removed dummy class call in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "DummyClass.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
-    std::cout << "DummyClass is invoked : " << DummyClass::canBeInvoked() << std::endl;
 
     return 0;
 }
